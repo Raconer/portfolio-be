65b6c5c6234b2d25cd25f131158c973dc57008e9
DongHoKim <cha11enge@hanmail.net>
2024-05-01 16:12:44 +0900

    first commit

---
+++ src/main/kotlin/com/portfolio/be/PortfolioBeApplication.kt
@@ -0,0 +1,11 @@
+package com.portfolio.be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioBeApplication>(*args)
+}
